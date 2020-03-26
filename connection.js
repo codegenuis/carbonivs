db0cc91dada903f724a0cad8926919270dc3623a
Charles Osegi <charlesosegi@gmail.com>
2020-03-26 10:29:41 +0100

    chamged database credentails

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql';
 
 var connection = mysql.createConnection({
-    host: '192.185.146.182',
-    user: 'wwmirayx_farmu1',
-    password: 'FAR)8%$@sg',
-    database: 'wwmirayx_farmersdb'
+    host: '192.232.194.4',
+    user: 'drugaida_farmer',
+    password: 'drugaida_farm',
+    database: 'drugaida_farm'
   })
 
   export default connection
